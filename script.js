a94414bba11309827bb3ea3284cecbc9200de644
pilu9147 <119696172+pilu9147@users.noreply.github.com>
2023-05-22 16:04:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let ans = document.getElementById("sizeInfo");
+ans.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+document.body.append(ans);
+
+
+window.onresize = function() {
+	ans.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+}
